28197e79408727b479cdb6210224655a2fefd92c
Felipe <felipeschutz2025@hotmail.com>
2024-01-11 17:25:56 -0300

    Added project's skeleton

---
+++ back-end/src/main/java/com/pacto/PactoApplication.java
@@ -0,0 +1,13 @@
+package com.pacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactoApplication.class, args);
+	}
+
+}
